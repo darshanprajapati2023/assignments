20636d8128f3670018192298cde7c90d4299b760
unknown <darshan2012.prajapati@gmail.com>
2024-01-26 21:07:25 +0530

    26-01-24

--- SE - Fundamentals of Programming/3.Looping Statements/2.cpp
+++ SE - Fundamentals of Programming/3.Looping Statements/2.cpp
@@ -1,13 +1,16 @@
 /*2. WAP to accept 5 numbers from user and display all numbers*/
 
-//#include<stdio.h>
-//int main(){
-//	
-//	int i=0;
-//	
-//	
-//	for (i=0;i<5;i++)
-//	{
-//		printf("\n%d",i);
-//	}
-//}
+#include<stdio.h>
+int main(){
+	
+	int i=0,n;
+	
+	
+	
+	for (i=0;i<5;i++)
+	{
+		printf("\nenter the number:");
+		scanf("%d",&n);
+		printf("\n%d",n);
+	}
+}
